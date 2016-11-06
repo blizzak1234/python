c470085cabd63f4047a210461532aa3224750741
blizzak1234 <botnikov@gmail.com>
2016-11-06 23:06:06 +0700

    файлы для контактов перемещены в пакеты

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,11 +1,8 @@
 # -*- coding: utf-8 -*-
-from selenium.webdriver.firefox.webdriver import WebDriver
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
-import unittest
 import pytest
-from contact import Contact
-from fixture.application import Application
 
+from fixture.application import Application
+from model.contact import Contact
 
 
 @pytest.fixture
