2e8f90b8eeea5daefb651084cefc70bb8141d418
blizzak1234 <botnikov@gmail.com>
2016-11-06 19:20:41 +0700

    перешел на py.test

--- test_add_group.py
+++ test_add_group.py
@@ -1,34 +1,23 @@
 # -*- coding: utf-8 -*-
-from selenium.webdriver.firefox.webdriver import WebDriver
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
-import unittest
+import pytest
 from group import Group
 from application import Application
 
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
-
-class test_add_group(unittest.TestCase):
-    def setUp(self):
-        self.app = Application()
-
-    def test_test_add_group(self):
-        self.app.login(username="admin", password="secret")
-        self.app.create_group(Group(name="group_name", header="group_logo", footer="group_comment"))
-        self.app.logout()
-
-    def test_test_add_empty_group(self):
-        self.app.login(username="admin", password="secret")
-        self.app.create_group(Group(name="", header="", footer=""))
-        self.app.logout()
-
-    def tearDown(self):
-        self.app.destroy()
+# функция инициализирующая фикстуру
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_test_add_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="group_name", header="group_logo", footer="group_comment"))
+    app.logout()
+
+
+def test_test_add_empty_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="", header="", footer=""))
+    app.logout()
