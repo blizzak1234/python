876409c88df52357b5376a1833a8b82a2c98b08e
blizzak1234 <botnikov@gmail.com>
2016-11-09 23:15:04 +0700

    переместил conftest в корень и удалил фикстуру из теста контакта

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,13 +1,6 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
 
-
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
-
 def test_add_contact(app):
     app.session.login(username="admin", password="secret")
     app.contact.create(Contact(F_name="FN", L_name="LN", C_address="contact_address", C_phone="02", C_email="email@fake.com"))
