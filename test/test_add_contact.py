776de77803a2b39b9320e6de1bcf1043482bd7af
blizzak1234 <botnikov@gmail.com>
2016-11-13 20:59:55 +0700

    вынес логин и логаут в фикстуру
    чтобы все тесты выполнялись в одной сессии

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -2,6 +2,4 @@
 from model.contact import Contact
 
 def test_add_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(F_name="FN", L_name="LN", C_address="contact_address", C_phone="02", C_email="email@fake.com"))
-    app.session.logout()
--- test/test_change_group.py
+++ test/test_change_group.py
@@ -1,6 +1,4 @@
 from model.group import Group
 
 def test_change_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.change(Group(name="group_name_ch", header="group_logo_ch", footer="group_comment_ch"))
-    app.session.logout()
