3a6da85b2b1359cb13025b3b993bd6750688ae5a
blizzak1234 <botnikov@gmail.com>
2016-11-13 23:39:39 +0700

    улучшил метод удаления группы
    теперь он проверяет есть ли группы для удаления

--- fixture/group.py
+++ fixture/group.py
@@ -83,3 +83,8 @@ class GroupHelper:
         # select first group
         wd = self.app.wd
         wd.find_element_by_name("selected[]").click()
+
+    def count(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        return len(wd.find_elements_by_name("selected[]"))
--- test/test_del_group.py
+++ test/test_del_group.py
@@ -1,4 +1,6 @@
-
+from model.group import Group
 
 def test_delete_first_group(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="77", header="77", footer="77"))
     app.group.delete_first_group()
