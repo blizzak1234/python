d48b97fbd108528c0d5f482a88d038399ed25987
blizzak1234 <botnikov@gmail.com>
2016-11-13 20:47:38 +0700

    создан тест с модификацией конкретных полей группы

--- test/test_change_group.py
+++ test/test_change_group.py
@@ -1,6 +1,6 @@
 from model.group import Group
 
-def test_test_change_group(app):
+def test_change_group(app):
     app.session.login(username="admin", password="secret")
     app.group.change(Group(name="group_name_ch", header="group_logo_ch", footer="group_comment_ch"))
     app.session.logout()
