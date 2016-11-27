e0e369095eb416bfff1b15dd62e72884c000a357
blizzak1234 <botnikov@gmail.com>
2016-11-27 20:31:27 +0700

    работают проверки для всех тестов контактов

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -1,6 +1,11 @@
 from model.contact import Contact
+from fixture.navigation import NavigationHelper
 
 def test_delete_contact(app):
     if app.contact.count() == False:
         app.contact.create(Contact(F_name="FN", L_name="LN", C_address="contact_address", C_phone="02", C_email="email@fake.com"))
+    old_contacts = app.contact.get_contact_list()
     app.contact.delete_contact()
+    app.navigation.return_to_home()
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) - 1 == len(new_contacts)
