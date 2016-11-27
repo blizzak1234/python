99f89fbe28f61c4abaaff957f83b0ecf7752fdad
blizzak1234 <botnikov@gmail.com>
2016-11-27 21:06:41 +0700

    добавил кэширование и хеширование в группы

--- fixture/group.py
+++ fixture/group.py
@@ -16,18 +16,20 @@ class GroupHelper:
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.fill_group_form(group)
+        # wd.find_element_by_name("group_name").click()
+        # wd.find_element_by_name("group_name").clear()
+        # wd.find_element_by_name("group_name").send_keys(group.name)
+        # wd.find_element_by_name("group_header").click()
+        # wd.find_element_by_name("group_header").clear()
+        # wd.find_element_by_name("group_header").send_keys(group.header)
+        # wd.find_element_by_name("group_footer").click()
+        # wd.find_element_by_name("group_footer").clear()
+        # wd.find_element_by_name("group_footer").send_keys(group.footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
+        self.group_cache = None
 
     def delete_first_group(self):
         wd = self.app.wd
@@ -36,7 +38,7 @@ class GroupHelper:
         # submit deletion
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
-
+        self.group_cache = None
 
     def return_to_groups_page(self):
         wd = self.app.wd
@@ -74,6 +76,7 @@ class GroupHelper:
         # submit modification
         wd.find_element_by_name("update").click()
         self.return_to_groups_page()
+        self.group_cache = None
 
     def select_first_group(self):
         # select first group
@@ -85,15 +88,18 @@ class GroupHelper:
         self.open_groups_page()
         return len(wd.find_elements_by_name("selected[]"))
 
+    group_cache = None
+
     def get_group_list(self):
-        wd = self.app.wd
-        self.open_groups_page()
-        groups = []
-        for element in wd.find_elements_by_css_selector("span.group"):
-            text = element.text
-            id = element.find_element_by_name("selected[]").get_attribute("value")
-            groups.append(Group(name=text, id=id))
-        return groups
+        if self.group_cache is None:
+            wd = self.app.wd
+            self.open_groups_page()
+            self.group_cache = []
+            for element in wd.find_elements_by_css_selector("span.group"):
+                text = element.text
+                id = element.find_element_by_name("selected[]").get_attribute("value")
+                self.group_cache.append(Group(name=text, id=id))
+            return list(self.group_cache) #возвращаем кэш. Лист для того чтобы кэш был неизменным. Если его испортили снаружи
 
 
 
--- test/test_del_group.py
+++ test/test_del_group.py
@@ -5,5 +5,4 @@ def test_delete_first_group(app):
         app.group.create(Group(name="77", header="77", footer="77"))
     old_groups = app.group.get_group_list()
     app.group.delete_first_group()
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) - 1 == len(new_groups)
+    assert len(old_groups) - 1 == app.group.count()
