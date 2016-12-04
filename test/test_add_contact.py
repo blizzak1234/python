3423f9f042d9788eba6a27709025d171bdc1aea5
blizzak1234 <botnikov@gmail.com>
2016-12-04 22:17:14 +0700

    Задание 15. Добавил параметризацию в создание сущностей

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -35,13 +35,13 @@ testdata = [Contact(F_name=random_string("fn", 5), L_name=random_string("ln", 5)
 
 
 @pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
-def test_add_contact(app):
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
     # contact = Contact(F_name="FN", L_name="LN", C_address="contact_address", H_phone="02", M_phone="5456",
     #                   W_phone="6543", S_phone="76543", C_email="email@fake.com")
-    app.contact.create(testdata)
+    app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
-    old_contacts.append(testdata)
+    old_contacts.append(contact)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
