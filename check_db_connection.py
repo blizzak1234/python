49b7e4879b580f5f5f7bfac96237ee4883519f2c
blizzak1234 <botnikov@gmail.com>
2016-12-13 19:57:06 +0700

    закончил лекции ORM

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,10 +1,11 @@
-import mysql.connector
+import pymysql.connections
 from fixture.orm import ORMFixture
+from model.group import Group
 
 db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
 
 try:
-    l = db.get_contact_list()
+    l = db.get_contacts_in_group(Group(id="21"))
     # cursor = connection.cursor()
     # cursor.execute("select * from group_list") # курсор стартует на таблице груплист
     # бежит по строкам и извлекает всю информацию в список
